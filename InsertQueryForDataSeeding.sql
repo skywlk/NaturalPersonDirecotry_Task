a8f1afe3328344a999ff5a1403523ce30525e40a
vache <vache8@gmail.com>
2019-12-14 18:04:53 +0400

    "imageUrlFix"

--- InsertQueryForDataSeeding.sql
+++ InsertQueryForDataSeeding.sql
@@ -1,11 +1,11 @@
 ﻿
 INSERT INTO [NPDDB].[NPD].[Persons] ([Firstname],[Lastname],[Gender],[PersonalNumber],[BirthDate],[CityId],[ImagePath])
 VALUES 
-('test1','test1',0,'02345678911','20001001',1,''),
-('test2','test2',0,'02345678912','20001001',1,''),
-('test3','test3',0,'02345678913','20001001',1,''),
-('test4','test4',0,'02345678914','20001001',1,''),
-('test5','test5',0,'12345678915','20001001',1,'')
+('test1','test1',0,'02345678911','20001001',1,'Images\image.jpg'),
+('test2','test2',0,'02345678912','20001001',1,'Images\image.jpg'),
+('test3','test3',0,'02345678913','20001001',1,'Images\image.jpg'),
+('test4','test4',0,'02345678914','20001001',1,'Images\image.jpg'),
+('test5','test5',0,'12345678915','20001001',1,'Images\image.jpg')
 
 INSERT INTO [NPDDB].[NPD].[RelatedPersons] ([Type],[PersonId],[RPersonId])
 VALUES 
